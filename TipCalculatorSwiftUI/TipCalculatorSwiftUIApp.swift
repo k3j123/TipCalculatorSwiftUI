0d7dd99e70a8538ed35342cf0e1b7e1bda961504
Trong <trong@atomicrobot.com>
2022-03-28 15:01:12 -0400

    Initial Commit

---
+++ TipCalculatorSwiftUI/TipCalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorSwiftUIApp.swift
+//  TipCalculatorSwiftUI
+//
+//  Created by Trong on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
